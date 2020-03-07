989601a5970b33d83b61b538e70ddfd28cb3134c
George Z. Zachos <gzachos@cse.uoi.gr>
2020-03-07 03:28:32 +0200

    Refactored code and added more synchronization
    Added synchronization where required and refactored code
    to facilitate new utilities.

--- door-monitor.c
+++ door-monitor.c
@@ -1,6 +1,6 @@
 /*
  * +-----------------------------------------------------------------------+
- * |             Copyright (C) 2017-2018 George Z. Zachos                  |
+ * |             Copyright (C) 2017-2020 George Z. Zachos                  |
  * +-----------------------------------------------------------------------+
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -35,7 +35,7 @@
 /* Global definitions */
 #define __STR(val)                       #val
 #define STR(val)                         __STR(val)
-#define MAIN_THREAD                      0
+#define NUM_THREADS                      1  // Number of joinable threads including main
 #define REED_SENSOR_PIN                  0
 #define BUZZER_PIN                       13
 #define ALERT_SWITCH_PIN                 2
@@ -49,44 +49,54 @@
 #define DOOR_OPENED(state,prev_state) \
                 (DOOR_WAS_CLOSED(prev_state) && !DOOR_IS_CLOSED(state))
 
+/* Global definitions */
 typedef struct timeval timeval_t;
 typedef struct thrarg_s {
-	timeval_t  req_timeval;
+	timeval_t timeval;
 	char      *state;
 } thrarg_t;
 
+enum thread_index {MAIN_THREAD};
+
 /* Function prototypes */
+long      get_curr_time(timeval_t *tv);
 long      get_timestamp(timeval_t tv);
-thrarg_t *alloc_arg(timeval_t req_timeval, char *state);
+thrarg_t *alloc_arg(timeval_t timeval, char *state);
 void     *notify_by_mail(void *arg);
 void      request_termination(int signo);
-void      terminate(int signo);
 void     *hit_buzzer(void *arg);
 int       timevalcmp(timeval_t tv1, timeval_t tv2);
 
-/* Global declarations */
-volatile sig_atomic_t termination_requested = 0, exit_signal = 0;
-volatile int exitflag = 0;
-volatile timeval_t latest_closed_door;
-pthread_mutex_t exitflag_mutex = PTHREAD_MUTEX_INITIALIZER;
-pthread_t tids[1];
+/* Global data */
+volatile sig_atomic_t termination_requested = 0,  // RW access only by main thread
+                      exit_signal = 0;            // RW access only by main thread
+volatile int exitflag = 0;   // Shared between all joinable threads
+volatile timeval_t latest_closed_door_tv; // Shared between all joinable threads
+pthread_mutex_t exitflag_mutex = PTHREAD_MUTEX_INITIALIZER,
+                closed_door_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_t tids[NUM_THREADS];
+
 
 int main(void)
 {
-	volatile int    curr_state,
+	volatile int    curr_state,  // Door state
 	                prev_state = -1;
 	pthread_attr_t  attr;
 	pthread_t       tid;
 	thrarg_t       *arg, *buzzer_arg;
-	timeval_t       req_timeval;
-	long            req_timestamp;
+	timeval_t       timeval;
+	long            timestamp;
 
 	openlog("door-monitor", LOG_PID, LOG_USER);
-	signal(SIGTERM, &request_termination);
-	signal(SIGINT,  &request_termination);
-	signal(SIGUSR1, &terminate);
+	timestamp = get_curr_time(&timeval);
+	syslog(LOG_INFO, "%ld: Door monitoring started", timestamp);
 
 	tids[MAIN_THREAD] = pthread_self();
+
+	signal(SIGTERM, &request_termination);
+	signal(SIGINT,  &request_termination);
+	signal(SIGUSR1, &request_termination);
+
 	pthread_attr_init(&attr);
 	pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED);
 
@@ -99,42 +109,54 @@ int main(void)
 	pinMode(ALERT_SWITCH_PIN, INPUT);
 	pinMode(MASTER_SWITCH_PIN, INPUT);
 
+	/* Monitor door state (open/closed) until termination is requested by signal */
 	for (; ;)
 	{
 		if (termination_requested)
 		{
 			if (pthread_mutex_trylock(&exitflag_mutex) == 0)
 			{
+				syslog(LOG_INFO, "Termination requested (%s)",
+						strsignal(exit_signal));
 				exitflag = termination_requested;
 				pthread_mutex_unlock(&exitflag_mutex);
 				break;
 			}
 		}
+
 		curr_state = digitalRead(REED_SENSOR_PIN);
-		gettimeofday(&req_timeval, NULL);
-		req_timestamp = get_timestamp(req_timeval);
+		timestamp = get_curr_time(&timeval);
+
 		if (DOOR_CLOSED(curr_state, prev_state))
 		{
-			latest_closed_door = req_timeval;
-			syslog(LOG_INFO, "%ld: Door closed\n", req_timestamp);
+			syslog(LOG_INFO, "%ld: Door closed\n", timestamp);
+
+			/* Store the most recent time the door closed */
+			pthread_mutex_lock(&closed_door_mutex);
+			latest_closed_door_tv = timeval;
+			pthread_mutex_unlock(&closed_door_mutex);
+
 			if (SWITCH_ISON(MASTER_SWITCH_PIN) ||
 					SWITCH_ISON(ALERT_SWITCH_PIN))
 			{
-				arg = alloc_arg(req_timeval, "closed");
+				/* Notify by email */
+				arg = alloc_arg(timeval, "closed");
 				pthread_create(&tid, &attr, &notify_by_mail,
 						(void *)arg);
 			}
 		}
-		if (DOOR_OPENED(curr_state, prev_state))
+		else if (DOOR_OPENED(curr_state, prev_state))
 		{
-			syslog(LOG_INFO, "%ld: Door opened\n", req_timestamp);
+			syslog(LOG_INFO, "%ld: Door opened\n", timestamp);
 			if (SWITCH_ISON(MASTER_SWITCH_PIN) ||
 					SWITCH_ISON(ALERT_SWITCH_PIN))
 			{
-				buzzer_arg = alloc_arg(req_timeval, "opened");
+				/* Sound buzzer */
+				buzzer_arg = alloc_arg(timeval, "opened");
 				pthread_create(&tid, &attr, &hit_buzzer,
 						(void *)buzzer_arg);
-				arg = alloc_arg(req_timeval, "opened");
+				/* Notify by email */
+				arg = alloc_arg(timeval, "opened");
 				pthread_create(&tid, &attr, &notify_by_mail,
 						(void *)arg);
 			}
@@ -143,9 +165,7 @@ int main(void)
 		delay((unsigned) 100);
 	}
 
-	pthread_mutex_lock(&exitflag_mutex);
-	syslog(LOG_INFO, "Termination requested (%s)", strsignal(exit_signal));
-	pthread_mutex_unlock(&exitflag_mutex);
+	syslog(LOG_INFO, "Termination completed\n");
 
 	pthread_attr_destroy(&attr);
 	closelog();
@@ -154,13 +174,31 @@ int main(void)
 }
 
 
-long get_timestamp(timeval_t tv)
+/* Get current time, store the corresponding timeval_t where tv_ptr
+ * points to and return timestamp in seconds.
+ */
+long get_curr_time(timeval_t *tv_ptr)
 {
-	return ((long) tv.tv_sec + (tv.tv_usec)*1.0E-06);
+	if (!tv_ptr)
+	{
+		syslog(LOG_INFO, "get_curr_time: NULL parameter provided\n");
+		pthread_kill(tids[MAIN_THREAD], SIGUSR1);
+	}
+
+	gettimeofday(tv_ptr, NULL);
+	return get_timestamp(*tv_ptr);
 }
 
 
-thrarg_t *alloc_arg(timeval_t req_timeval, char *state)
+/* Convert timeval_t to timestamp in seconds */
+long get_timestamp(timeval_t tv)
+{
+	return tv.tv_sec + (tv.tv_usec * 1.0E-06);
+}
+
+
+/* Allocate thread argument */
+thrarg_t *alloc_arg(timeval_t tv, char *state)
 {
 	thrarg_t *arg;
 
@@ -170,26 +208,27 @@ thrarg_t *alloc_arg(timeval_t req_timeval, char *state)
 		syslog(LOG_INFO, "malloc: %s", strerror(errno));
 		return NULL;
 	}
-	arg->req_timeval = req_timeval;
+	arg->timeval = tv;
 	arg->state = state;
 	return arg;
 }
 
 
+/* Executed by detached thread */
 void *notify_by_mail(void *arg)
 {
-	char      cmd[256], *state;
-	int       ret, attempts;
-	long      req_timestamp;
+	char cmd[256], *state;
+	int  ret, attempts;
+	long timestamp;
 
 	if (!arg)
 		return ((void *) EXIT_FAILURE);
 
-	req_timestamp = get_timestamp(((thrarg_t *) arg)->req_timeval);
+	timestamp = get_timestamp(((thrarg_t *) arg)->timeval);
 	state = ((thrarg_t *) arg)->state;
 
 	snprintf(cmd, 256, "%s/door-sendmail.sh %s %ld", STR(TARGET_DIR),
-		state, req_timestamp);
+		state, timestamp);
 	for (attempts = 10; attempts > 0; attempts--)
 	{
 		ret = system(cmd);
@@ -199,13 +238,13 @@ void *notify_by_mail(void *arg)
 			if (ret == 0)
 			{
 				syslog(LOG_INFO, "%ld: Mail sent (%s)\n",
-					req_timestamp, state);
+					timestamp, state);
 				free(arg);
 				return ((void *) EXIT_SUCCESS);
 			}
 		}
 		syslog(LOG_INFO, "%ld: Attempt to send mail #%d/9\n",
-			req_timestamp, 10-attempts);
+			timestamp, 10-attempts);
 		pthread_yield();
 		delay((unsigned) 500);
 	}
@@ -215,34 +254,37 @@ void *notify_by_mail(void *arg)
 }
 
 
+/* Signal handler for SIGINT, SIGTERM and SIGUSR1 */
 void request_termination(int signo)
 {
-	exit_signal = signo;
+	/* Make sure only the main thread accesses exit_signal
+	 * and termination_requested variables, as no synchronization
+	 * can take place inside the signal handler to avoid race conditions.
+	 */
 	if (pthread_self() == tids[MAIN_THREAD])
-		terminate(SIGUSR1);
+	{
+		exit_signal = signo;
+		termination_requested = 1;
+	}
 	else
 		pthread_kill(tids[MAIN_THREAD], SIGUSR1);
 }
 
 
-void terminate(int signo)
-{
-	termination_requested = 1;
-}
-
-
+/* Executed by detached thread */
 void *hit_buzzer(void *arg)
 {
-	int i;
-	long      req_timestamp;
-	timeval_t opendoor_time;
+	int       i;
+	timeval_t opendoor_tv;
+	long      opendoor_time;
 
 	if (!arg)
 		return ((void *) EXIT_FAILURE);
 
-	opendoor_time = ((thrarg_t *) arg)->req_timeval;
-	req_timestamp = get_timestamp(opendoor_time);
+	opendoor_tv = ((thrarg_t *) arg)->timeval;
+	opendoor_time = get_timestamp(opendoor_tv);
 
+	/* Sound the buzzer three (3) times for one (1) second */
 	for (i = 0; i < 3; i++)
 	{
 		digitalWrite(BUZZER_PIN, HIGH);
@@ -251,11 +293,28 @@ void *hit_buzzer(void *arg)
 		delay((unsigned) 500);
 	}
 
-	for (i = 0; timevalcmp(latest_closed_door, opendoor_time) == -1; i++)
+
+	for (i = 0; ; i++)
 	{
+		/* Check if the door has closed since this hit_buzzer instance
+		 * was called. Variable latest_closed_door_tv is shared between
+		 * all threads and synchronization is required to access it.
+		 */
+		pthread_mutex_lock(&closed_door_mutex);
+		if (timevalcmp(opendoor_tv, latest_closed_door_tv) < 0)
+		{
+			pthread_mutex_unlock(&closed_door_mutex);
+			break;
+		}
+		pthread_mutex_unlock(&closed_door_mutex);
+
+		/* Sound the buzzer one (1) time for five (5) seconds every
+		 * approximately five (5) minutes as a reminder that the door
+		 * is still open.
+		 */
 		if (i == 60*5)
 		{
-			syslog(LOG_INFO, "%ld: Door is still open\n", req_timestamp);
+			syslog(LOG_INFO, "%ld: Door is still open\n", opendoor_time);
 			digitalWrite(BUZZER_PIN, HIGH);
 			delay((unsigned) 1000*5);
 			digitalWrite(BUZZER_PIN, LOW);
@@ -269,6 +328,7 @@ void *hit_buzzer(void *arg)
 }
 
 
+/* Precise timeval_t comparator */
 int timevalcmp(timeval_t tv1, timeval_t tv2)
 {
 	if (tv1.tv_sec < tv2.tv_sec)
@@ -281,5 +341,4 @@ int timevalcmp(timeval_t tv1, timeval_t tv2)
 		return (1);
 	return (0);
 }
-
 
